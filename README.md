7c1c9698f003f703cacda66d4892ed544df23f1f
SolCaruso <caruso@ocelot.ltd>
2024-04-29 23:45:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+**To Do's**
+
+[ ] - Replace all code on perks.html with tw classes
+[ ] - Replace all code on revolver.html with tw classes
+[ ] - Replace all code on index.html with tw classes
+[ ] - Update sportsbook buttons 
+[ ] - Correct all active page nav buttons
+[ ] - Create better look for active and hover effect for Menu drop down
+[ ] - Fix drop-down active and non active button
+[ ] - Create slider functionality
+[ ] - Create different slides
+[ ] - Replace all images with .webp format
+[ ] - Add propper responsive image resizing
+[ ] - Clean up index.html look
+[ ] - Add paralax effect with WildHart
---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        rvr: "linear-gradient(to right, rgba(255, 73, 77, 1), rgba(255, 111, 110, 1))",
+      }),
+      fontSmoothing: ["antialiased"],
+      textTransform: {
+        initial: "initial",
+      },
+      letterSpacing: {
+        initial: "initial",
+      },
+      colors: {
+        "sldr-green": "#A9BBA8",
+        "sldr-2nd-green": "#92A591",
+        "rarity-purple": "#9867EF",
+        "rarity-black": "#333832",
+        "rvr-red": "#FF5457",
+        "rvr-2": "#FF5457",
+        "rvr-bright": "#F53838",
+        "rvr-hover": "#ff0101",
+        "rvr-hover-2": "#FF6A69",
+        "rvr-hover-3": "#FFBABA",
+        "rvr-bg": "rgb(255, 71, 76, 0.25)",
+        "rvr-box": "#FF6E6D",
+        "rvr-grey": "#828282",
+        "rvr-full-bg": "#F8F8F8",
+        "nav-grey": "rgba(0, 0, 0, 0.05)",
+        "nav-grey-hover": "rgba(0, 0, 0, 0.10)",
+        "nav-red": "rgba(192, 53, 64, 1)",
+      },
+      fontSize: {
+        "2xs": ".65rem",
+        "3xs": ".55rem",
+        "4xs": ".45rem",
+        "5xs": ".35rem",
+        "6xs": ".25rem",
+      },
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "system-ui",
+          "BlinkMacSystemFont",
+          "San Francisco",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+      screens: {
+        xs: "440px",
+        "2xs": "340px",
+        nav: "850px",
+      },
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
