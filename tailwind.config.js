6f91d367e70b26bf7eff4e8c1c7d6228f2383c19
SolCaruso <caruso@ocelot.ltd>
2024-07-02 00:07:30 -0400

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}",
-  './index.html',
-  './nft.html',
+  content: ["./**/*.{html,js}"
 ],
   theme: {
     extend: {
